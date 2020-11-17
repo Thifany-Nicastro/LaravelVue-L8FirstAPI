e994025033d2b53bd5378168d86f7e7e45bf06e9
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-11-17 11:35:57 -0300

    List, Create

---
+++ front/src/views/products/Create.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card text-center">
+                    <div class="card-header">Create product</div>
+
+                    <div class="card-body">
+                        <form>
+                            <div class="row mb-3 justify-content-center">
+                                <label for="name" class="col-md-2 col-form-label text-md-right">Name</label>
+
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" id="name" v-model="product.name">
+                                </div>
+                            </div>
+                            <div class="row mb-3 justify-content-center">
+                                <label for="description" class="col-md-2 col-form-label text-md-right">Description</label>
+
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" id="description" v-model="product.description">
+                                </div>
+                            </div>
+                            <div class="row mb-3 justify-content-center">
+                                <label for="value" class="col-md-2 col-form-label text-md-right">Value</label>
+
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" id="value" v-model="product.value">
+                                </div>
+                            </div>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                <button class="btn btn-primary" type="button" @click.prevent="store()">Button</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    axios.defaults.baseURL = "http://localhost:8000/api";
+
+    export default {
+        data() {
+            return {
+                product: {
+                    name: '',
+                    description: '',
+                    value: ''
+                }
+            }
+        },
+
+        methods: {
+            reset () {
+                this.product = {
+                    name: '',
+                    description: '',
+                    value: ''
+                }
+            },
+
+            store () {
+                axios.post('/products', this.product).then(response => {
+                    console.log(response)
+                })
+                this.reset();
+            },
+        }
+    }
+</script>
---
+++ front/src/views/products/Index.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Products</div>
+
+                    <div class="card-body">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Category</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="product in products" v-bind:key="product">
+                                    <td>{{ product.name }}</td>
+                                    <td>{{ product.description }}</td>
+                                    <td>{{ product.value }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    axios.defaults.baseURL = "http://localhost:8000/api";
+
+    export default {
+        data() {
+            return {
+                products: []
+            }
+        },
+
+        mounted () {
+            this.all();
+        },
+
+        methods: {
+            all () {
+                axios.get('/products').then(response => {
+                  this.products = response.data.products
+               })
+            }
+        }
+    }
+</script>
